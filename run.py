75d94bb78034e22a26816bb28f719826b7f05586
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2025-06-25 09:25:47 -0300

    Salva alterações locais antes do rebase t push origin main

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-from flask import Flask, redirect, url_for, has_request_context
-from app.extensions import db, login_manager
-from flask_migrate import Migrate
+from app import create_app
+import os
 
-# Apenas esta linha fora da função (para registrar os modelos no alembic)
-from app import models
-from app.models import RequisicaoTecnico
-from flask_login import current_user
+app = create_app()
 
-# Importa todos os comandos CLI do app.cli
-from app.cli import init_db, seed_dados, criar_usuario, editar_usuario, listar_usuarios, deletar_usuario
+with app.app_context():
+    from app.extensions import db
+    db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
