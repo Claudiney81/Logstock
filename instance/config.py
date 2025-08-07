e53e25d1e6a8c566a892268f28c2dd4e9612dde2
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2025-08-07 16:35:05 -0300

    Atualiza config.py com URL do banco PostgreSQL da Render

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,12 @@
 import os
+from urllib.parse import quote_plus
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'logistock.db')
+# URL do banco PostgreSQL hospedado na Render
+SQLALCHEMY_DATABASE_URI = 'postgresql://logstock_db_user:nErIcMmMHmkHXTKcJVYYm1MU2TWUIkUq@dpg-d2afshk9c44c738qvek0-a.oregon-postgres.render.com/logstock_db'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'logistock123'
+
+# Chave secreta para sessões Flask
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'logistock123'
