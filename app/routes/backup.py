f86ae9ab1cb064eabef0ca2f316c0a61ac3da0d7
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2026-06-07 18:58:24 -0300

    Ajusta caminho backup drive

--- app/routes/backup.py
+++ app/routes/backup.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Blueprint, jsonify
 from flask_login import login_required
-import os
+
+from app.utils.backup_drive import enviar_backup_google_drive
 
 bp_backup = Blueprint(
     "backup",
@@ -13,7 +16,24 @@ bp_backup = Blueprint(
 @login_required
 def executar_backup():
 
-    return jsonify({
-        "cwd": os.getcwd(),
-        "arquivos": os.listdir(".")
-    })
+    try:
+        caminho_banco = os.path.join(
+            os.getcwd(),
+            "logistock.db"
+        )
+
+        arquivo_id = enviar_backup_google_drive(
+            caminho_banco
+        )
+
+        return jsonify({
+            "status": "ok",
+            "mensagem": "Backup enviado para o Google Drive",
+            "arquivo_id": arquivo_id
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "erro",
+            "mensagem": str(e)
+        }), 500
