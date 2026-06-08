7307a6615af084dd328e11e2f5718dc122836b38
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2026-06-08 18:31:38 -0300

    Remove mensagem automatica do Flask Login

--- app/__init__.py
+++ app/__init__.py
@@ -83,6 +83,7 @@ def create_app():
 
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
+    login_manager.login_message = None
     login_manager.login_message_category = "info"
 
     @login_manager.user_loader
